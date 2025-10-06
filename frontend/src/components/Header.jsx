851d9cc2737be99d91313347c96c60baf324efd8
Hasintha Pasindu <Hasinthapasindu20@gmail.com>
2025-10-07 00:22:41 +0530

    feat: enhance Header component

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './Header.css';
 
 const Header = () => {
@@ -8,14 +8,14 @@ const Header = () => {
       <nav>
         <h1>HeavySync - Supplier & Purchase Order Management</h1>
         <ul className="nav-menu">
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/suppliers">Suppliers</Link></li>
-          <li><Link to="/suppliers/new">Add Supplier</Link></li>
-          <li><Link to="/parts">Parts Inventory</Link></li>
-          <li><Link to="/parts/new">Add Part</Link></li>
-          <li><Link to="/purchase-orders">Purchase Orders</Link></li>
-          <li><Link to="/purchase-orders/new">Create Order</Link></li>
-          <li><Link to="/quotations">Compare Quotations</Link></li>
+          <li><NavLink to="/" end>Home</NavLink></li>
+          <li><NavLink to="/suppliers" end>Suppliers</NavLink></li>
+          <li><NavLink to="/suppliers/new">Add Supplier</NavLink></li>
+          <li><NavLink to="/parts" end>Parts Inventory</NavLink></li>
+          <li><NavLink to="/parts/new">Add Part</NavLink></li>
+          <li><NavLink to="/purchase-orders" end>Purchase Orders</NavLink></li>
+          <li><NavLink to="/purchase-orders/new">Create Order</NavLink></li>
+          <li><NavLink to="/quotations">Compare Quotations</NavLink></li>
         </ul>
       </nav>
     </header>
