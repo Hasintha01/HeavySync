075ab93ea5f8b0556dd5b12fefe4f43c2a0dea45
Hasintha Pasindu <Hasinthapasindu20@gmail.com>
2025-10-07 15:00:52 +0530

    feat: improve email validation

--- frontend/src/modules/supplier/pages/SupplierForm.jsx
+++ frontend/src/modules/supplier/pages/SupplierForm.jsx
@@ -94,7 +94,7 @@ const SupplierForm = ({ onSuccess }) => {
     if (!email.includes('@')) {
       return false;
     }
-    const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    const emailRegex = /^\w+([-.]?\w+)*@\w+([-.]?\w+)*(\.\w{2,3})+$/;
     return emailRegex.test(email);
   };
 
